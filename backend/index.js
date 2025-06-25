868b04076874076a3fcc2d401c155c5a02ab6ee7
RahulSinghklr <rahulshiksingh@gmail.com>
2025-06-26 00:15:41 +0530

    new comment added 9

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-//just to check i m 
+
 const PORT = process.env.PORT || 5000;
 const productRoutes = require("./routes/productRoutes");
 
