4615b69b15c9de7adeb226f6f11f143bc820261a
RahulSinghklr <rahulshiksingh@gmail.com>
2025-06-26 00:16:20 +0530

    Error in code

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+3333333333333333333333
 const PORT = process.env.PORT || 5000;
 const productRoutes = require("./routes/productRoutes");
 
